28c1a7a47857ccba4f8b72c7bcbc022da5120cd3
ps4boot <ps4boot@localhost.mail>
2020-12-30 18:33:32 +0100

    6.72offsets

--- kernel.c
+++ kernel.c
@@ -254,8 +254,8 @@ static int patch_pmap_check(void)
 
     for (p = (u8*)kern.pmap_protect;
          p < ((u8*)kern.pmap_protect + 0x500); p++) {
-        #ifdef PS4_6_20 // changed to 62 from 74
-        if (!memcmp(p, "\xB8\x06\x00\x00\x00\xC4", 6)) {
+        #ifdef PS4_6_72 // changed to 62 from 74
+        if (!memcmp(p, "\xF8\xF7\xD0\x83\xE0\x06", 6)) {
             p[1] = 0;
             kern.printf("pmap_protect patch successful (found at %p)\n", p);
             return 1;
